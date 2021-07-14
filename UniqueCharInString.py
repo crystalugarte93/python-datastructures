9e9933388e11b2add6f1a00470aecbbad4aa12db
Crystal Ugarte <crystal.ugarte@klarna.com>
2021-07-14 21:15:48 +0200

    final function of the class done

--- UniqueCharInString.py
+++ UniqueCharInString.py
@@ -1,13 +1,22 @@
+def uni_char(s):
+    return print(len(set(s)) == len(s))
 
 def unique_char_in_string(s):
     # Given a string, determine if it is comprised of all unique characters.
     # e.g. 'abcde' has all unique characters and should return True
     # 'aabcde' should return False
-    r =''
+    char = set() # empty seet
 
+    for let in s:
+        if let in char:
+            return print(False)
+        else:
+            char.add(let)
+    return print(True)
 
 def main():
-    s = 'sfbnsas'
+    s = 'sfbn'
+    uni_char(s)
     unique_char_in_string(s)
 
 
